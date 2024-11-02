17c1e2d31f1592cae9f4ba248246e30582f6f4a1
Oscar Luque Porca <oscarluqueporca@gmail.com>
2024-11-02 13:04:23 +0100

    Reto_Conjunto_2

--- src/main/java/org/example/reto_conjunto/dao/UsuarioDAO.java
+++ src/main/java/org/example/reto_conjunto/dao/UsuarioDAO.java
@@ -1,10 +1,15 @@
 package org.example.reto_conjunto.dao;
 
 import org.example.reto_conjunto.models.Usuario;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 import java.util.List;
 
+
 public class UsuarioDAO implements DAO<Usuario> {
+    private SessionFactory sessionFactory;
     @Override
     public List<Usuario> findAll() {
         return List.of();
@@ -28,5 +33,19 @@ public class UsuarioDAO implements DAO<Usuario> {
     @Override
     public void delete(Usuario usuario) {
 
+    }
+
+    public Usuario validateUser(String email, String password){
+        try(Session session = sessionFactory.openSession()){
+            Query<Usuario> q = session.createQuery("select u from Usuario u  where u.email = :email and u.contrasena = :contrasena");
+            q.setParameter("email", email);
+            q.setParameter("password", password);
+
+            Usuario user = q.uniqueResult();
+            Usuario user1 = session.get(Usuario.class, user.getId());
+            user1.getCopias();
+
+            return user;
+        }
     }
 }
