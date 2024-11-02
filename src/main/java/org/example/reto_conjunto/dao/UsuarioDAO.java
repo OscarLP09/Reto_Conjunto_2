a64579ef3a4b49a6993d550bb0461a2dc87ba594
Oscar Luque Porca <oscarluqueporca@gmail.com>
2024-11-02 12:57:55 +0100

    Reto_Conjunto_2

--- src/main/java/org/example/reto_conjunto/dao/UsuarioDAO.java
+++ src/main/java/org/example/reto_conjunto/dao/UsuarioDAO.java
@@ -1,4 +1,32 @@
 package org.example.reto_conjunto.dao;
 
-public class UsuarioDAO {
+import org.example.reto_conjunto.models.Usuario;
+
+import java.util.List;
+
+public class UsuarioDAO implements DAO<Usuario> {
+    @Override
+    public List<Usuario> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Usuario findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void save(Usuario usuario) {
+
+    }
+
+    @Override
+    public void update(Usuario usuario) {
+
+    }
+
+    @Override
+    public void delete(Usuario usuario) {
+
+    }
 }
